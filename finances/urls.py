e7b9888287b5a989254697a1e5c2a3df8da55b9f
Teiji Watanabe <teijiwatanabe01@gmail.com>
2020-10-25 01:22:34 -0400

    Project name update

--- finances/urls.py
+++ finances/urls.py
@@ -6,8 +6,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("core/", include("apps.core.urls")),
-    path("page/", include("apps.page.urls")),
-    path("budget/", include("apps.budget.urls")),
-    path("expense/", include("apps.expense.urls")),
-    path("api-token-auth/", obtain_auth_token),
+    path("api/page/", include("apps.page.urls")),
+    path("api/budget/", include("apps.budget.urls")),
+    path("api/expense/", include("apps.expense.urls")),
+    path("api/api-token-auth/", obtain_auth_token),
 ]
--- finances/wsgi.py
+++ finances/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for finances_api project.
+WSGI config for finances project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finances_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finances.settings')
 
 application = get_wsgi_application()
